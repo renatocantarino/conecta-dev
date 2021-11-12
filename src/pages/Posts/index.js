bf1ffebda80e9796d14396a972d916da47243fa0
renatocantarino <renato.cantarino@gmail.com>
2021-11-12 12:54:43 -0300

    changes

--- src/pages/Posts/index.js
+++ src/pages/Posts/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Header from '../../components/Header';
+import Box from '@material-ui/core/Box';
 
 
 function NewPost() {
     return (
         <>
             <Header />
+            <Box mt={13} display="flex" height="calc(100% - 70px)" overflow="scroll">
+                <Box width="50%" height="100%" padding={2} borderRight="1px solid #DDD">
+                    sasasa
+                </Box>
+                <Box width="50%" height="100%" padding={2}>
+                    xsxsxsx
+                </Box>
+            </Box>
 
         </>
     );
